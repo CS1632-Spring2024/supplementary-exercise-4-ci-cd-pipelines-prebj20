81a21767a665c75969eec57830548bbc6e9614bf
Joel Prebish <prebj20@gmail.com>
2024-04-05 13:59:06 -0400

    revert

--- src/main/java/edu/pitt/cs/Cat.java
+++ src/main/java/edu/pitt/cs/Cat.java
@@ -1,7 +1,7 @@
-<<<<<<< HEAD
 package edu.pitt.cs;
 
-import org.mockito.*;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.*;
 
 public interface Cat {
 	public static Cat createInstance(InstanceType type, int id, String name) {
@@ -13,9 +13,8 @@ public interface Cat {
 			case SOLUTION:
 				return new CatSolution(id, name);
 			case MOCK:
-				Cat cat = Mockito.mock(Cat.class);
 				// TODO: Return a mock object that emulates the behavior of a real object.
-				return cat; // come back to this
+				return null;
 			default:
 				assert (false);
 				return null;
@@ -39,45 +38,3 @@ public interface Cat {
 
 	public String toString();
 }
-=======
-package edu.pitt.cs;
-
-import org.mockito.*;
-
-public interface Cat {
-	public static Cat createInstance(InstanceType type, int id, String name) {
-		switch (type) {
-			case IMPL:
-				return new CatImpl(id, name);
-			case BUGGY:
-				return new CatBuggy(id, name);
-			case SOLUTION:
-				return new CatSolution(id, name);
-			case MOCK:
-				Cat cat = Mockito.mock(Cat.class);
-				// TODO: Return a mock object that emulates the behavior of a real object.
-				return cat; // come back to this
-			default:
-				assert (false);
-				return null;
-		}
-	}
-
-	// WARNING: You are not allowed to change any part of the interface.
-	// That means you cannot add any method nor modify any of these methods.
-
-	public void rentCat();
-
-	public void returnCat();
-
-	public void renameCat(String name);
-
-	public String getName();
-
-	public int getId();
-
-	public boolean getRented();
-
-	public String toString();
-}
->>>>>>> 2adf44aee1f252281d577042bdd5b14ab49bffe4
--- src/main/java/edu/pitt/cs/RentACat.java
+++ src/main/java/edu/pitt/cs/RentACat.java
@@ -1,7 +1,7 @@
-<<<<<<< HEAD
 package edu.pitt.cs;
 
 import org.mockito.Mockito;
+import static org.mockito.Mockito.*;
 
 public interface RentACat {
 	public static RentACat createInstance(InstanceType type) {
@@ -13,9 +13,8 @@ public interface RentACat {
 			case SOLUTION:
 				return new RentACatSolution();
 			case MOCK:
-				RentACat c = Mockito.mock(RentACat.class);
 				// TODO: Return a mock object that emulates the behavior of a real object.
-				return c; // come back to this mock!!
+				return null;
 			default:
 				assert (false);
 				return null;
@@ -35,41 +34,3 @@ public interface RentACat {
 
 	public void addCat(Cat c);
 }
-=======
-package edu.pitt.cs;
-
-import org.mockito.Mockito;
-
-public interface RentACat {
-	public static RentACat createInstance(InstanceType type) {
-		switch (type) {
-			case IMPL:
-				return new RentACatImpl();
-			case BUGGY:
-				return new RentACatBuggy();
-			case SOLUTION:
-				return new RentACatSolution();
-			case MOCK:
-				RentACat c = Mockito.mock(RentACat.class);
-				// TODO: Return a mock object that emulates the behavior of a real object.
-				return c; // come back to this mock!!
-			default:
-				assert (false);
-				return null;
-		}
-	}
-
-	// WARNING: You are not allowed to change any part of the interface.
-	// That means you cannot add any method nor modify any of these methods.
-
-	public boolean returnCat(int id);
-
-	public boolean rentCat(int id);
-
-	public boolean renameCat(int id, String name);
-
-	public String listCats();
-
-	public void addCat(Cat c);
-}
->>>>>>> 2adf44aee1f252281d577042bdd5b14ab49bffe4
